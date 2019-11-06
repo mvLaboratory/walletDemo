abbf2f66279f10e839d81d5e8bf7d5b0d63eb133
Volodymyr Mykytiuk <mvLabaratory@gmail.com>
2019-11-07 01:29:46 +0200

    header added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React from "react";
 
 import Balance from "./Components/Balance/Balance.js";
 import Footer from "./Components/Layouts/Footer.js";
+import Header from "./Components/Layouts/Header.js";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="HeaderInfo">
-          <img src={logo} className="App-logo" alt="logo" />
-          <span className="AppName">Personal Wallet!</span>
-        </div>
-      </header>
+      <Header />
       <body>
         <div className="BodyContainer">
           <Balance />
--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
 
 const useStyles = makeStyles({
     root: {
-      width: "100%"
-    },
+      width: "100%",
+    }
   });
 
   export default function Footer() {
@@ -25,9 +23,7 @@ const useStyles = makeStyles({
         showLabels
         className={classes.root}
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction label="Balance" icon={<AccountBalanceWalletIcon color="primary"/>} />
       </BottomNavigation>
     );
   }
